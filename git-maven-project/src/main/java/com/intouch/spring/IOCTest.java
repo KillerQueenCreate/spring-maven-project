f2e08d61bb617d07c81531aa2a4ac5023028c376
luna <1989158472@qq.com>
2021-05-14 12:12:22 +0800

    leader创建了spring maven工程

---
+++ git-maven-project/src/main/java/com/intouch/spring/IOCTest.java
@@ -0,0 +1,22 @@
+package com.intouch.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class IOCTest {
+    private static final DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+    public static void main(String[] args) {
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        //Date now= (Date) context.getBean("now");
+        Date today=(Date) context.getBean("now");
+        System.out.println("Now is "+ today);
+
+
+    }
+
+}
